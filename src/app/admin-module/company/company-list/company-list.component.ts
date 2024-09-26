a7d69b79e17e6496369f5bf74cd98a81ef7fa6dc
Umair Tahir <aleem.ahmad@cloudtek.biz>
2024-09-27 01:16:11 +0500

    install angular material and integrate company dialog

--- src/app/admin-module/company/company-list/company-list.component.ts
+++ src/app/admin-module/company/company-list/company-list.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
 import { Company } from '../../../types';
+import { CompanyModalComponent } from "../company-modal/company-modal.component";
 
 @Component({
   selector: 'app-company-list',
@@ -8,90 +10,126 @@ import { Company } from '../../../types';
   templateUrl: './company-list.component.html',
   styleUrl: './company-list.component.css'
 })
-export class CompanyListComponent {
+export class CompanyListComponent implements OnInit {
 
-companyList: Company[] = [
-  {
-    "id": 1,
-    "title": "Your Company",
-    "address": "",
-    "type": "Company",
-    "country": "United States",
-    "timezone": "Europe/London",
-    "parent": null
-  },
-  {
-    "id": 2,
-    "title": "Head Office",
-    "address": "PO Box 001002\nSample Road, Sample Town",
-    "type": "Head Office",
-    "country": "United States",
-    "timezone": "Europe/London",
-    "parent": "Your Company"
-  },
-  {
-    "id": 3,
-    "title": "Marketing Department",
-    "address": "PO Box 001002\nSample Road, Sample Town",
-    "type": "Department",
-    "country": "United States",
-    "timezone": "Europe/London",
-    "parent": "Head Office"
-  },
-  {
-    "id": 4,
-    "title": "Development Center",
-    "address": "PO Box 001002\nSample Road, Sample Town",
-    "type": "Regional Office",
-    "country": "Singapore",
-    "timezone": "Europe/London",
-    "parent": "Your Company"
-  },
-  {
-    "id": 5,
-    "title": "Engineering Department",
-    "address": "PO Box 001002\nSample Road, Sample Town,  341234",
-    "type": "Department",
-    "country": "Singapore",
-    "timezone": "Europe/London",
-    "parent": "Development Center"
-  },
-  {
-    "id": 6,
-    "title": "Development Team",
-    "address": "",
-    "type": "Unit",
-    "country": "Singapore",
-    "timezone": "Europe/London",
-    "parent": "Engineering Department"
-  },
-  {
-    "id": 7,
-    "title": "QA Team",
-    "address": "",
-    "type": "Unit",
-    "country": "Singapore",
-    "timezone": "Europe/London",
-    "parent": "Engineering Department"
-  },
-  {
-    "id": 8,
-    "title": "Server Administration",
-    "address": "",
-    "type": "Unit",
-    "country": "Singapore",
-    "timezone": "Europe/London",
-    "parent": "Engineering Department"
-  },
-  {
-    "id": 9,
-    "title": "Administration & HR",
-    "address": "",
-    "type": "Department",
-    "country": "Singapore",
-    "timezone": "Europe/London",
-    "parent": "Development Center"
+
+  constructor(
+    private dialog: MatDialog,
+  ) {
+
   }
-];
+
+
+  companyList: Company[] = [
+    {
+      "id": 1,
+      "title": "Your Company",
+      "address": "",
+      "type": "Company",
+      "country": "United States",
+      "timezone": "Europe/London",
+      "parent": null
+    },
+    {
+      "id": 2,
+      "title": "Head Office",
+      "address": "PO Box 001002\nSample Road, Sample Town",
+      "type": "Head Office",
+      "country": "United States",
+      "timezone": "Europe/London",
+      "parent": "Your Company"
+    },
+    {
+      "id": 3,
+      "title": "Marketing Department",
+      "address": "PO Box 001002\nSample Road, Sample Town",
+      "type": "Department",
+      "country": "United States",
+      "timezone": "Europe/London",
+      "parent": "Head Office"
+    },
+    {
+      "id": 4,
+      "title": "Development Center",
+      "address": "PO Box 001002\nSample Road, Sample Town",
+      "type": "Regional Office",
+      "country": "Singapore",
+      "timezone": "Europe/London",
+      "parent": "Your Company"
+    },
+    {
+      "id": 5,
+      "title": "Engineering Department",
+      "address": "PO Box 001002\nSample Road, Sample Town,  341234",
+      "type": "Department",
+      "country": "Singapore",
+      "timezone": "Europe/London",
+      "parent": "Development Center"
+    },
+    {
+      "id": 6,
+      "title": "Development Team",
+      "address": "",
+      "type": "Unit",
+      "country": "Singapore",
+      "timezone": "Europe/London",
+      "parent": "Engineering Department"
+    },
+    {
+      "id": 7,
+      "title": "QA Team",
+      "address": "",
+      "type": "Unit",
+      "country": "Singapore",
+      "timezone": "Europe/London",
+      "parent": "Engineering Department"
+    },
+    {
+      "id": 8,
+      "title": "Server Administration",
+      "address": "",
+      "type": "Unit",
+      "country": "Singapore",
+      "timezone": "Europe/London",
+      "parent": "Engineering Department"
+    },
+    {
+      "id": 9,
+      "title": "Administration & HR",
+      "address": "",
+      "type": "Department",
+      "country": "Singapore",
+      "timezone": "Europe/London",
+      "parent": "Development Center"
+    }
+  ];
+
+
+
+
+  ngOnInit(): void {
+
+  }
+
+  openCreateCompanyModal() {
+    const dialogRef = this.dialog.open(CompanyModalComponent, {
+      disableClose: true,
+      data: {},
+      maxWidth: '70vw',
+      width: '100%',
+    });
+    dialogRef.afterClosed().subscribe(_ => { if (_ === undefined) this.ngOnInit(); });
+  }
+
+  openEditCompanyModal(company: {}) {
+    const dialogRef = this.dialog.open(CompanyModalComponent, {
+      disableClose: true,
+      data: company,
+      maxWidth: '70vw',
+      width: '100%',
+    });
+    dialogRef.afterClosed().subscribe(_ => { if (_ === undefined) this.ngOnInit(); });
+  }
+
 
 }
--- src/app/admin-module/company/company-modal/company-modal.component.ts
+++ src/app/admin-module/company/company-modal/company-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-company-modal',
@@ -8,5 +10,70 @@ import { Component } from '@angular/core';
   styleUrl: './company-modal.component.css'
 })
 export class CompanyModalComponent {
+  createCompanyForm!: FormGroup;
+  projectTypes: any;
+  emailList: string[] = []
+  createProjectResult: any;
+  maxDate = new Date();
+  @Input()
+  align!: 'start' | 'center' | 'end'
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private fb: FormBuilder, public dialogRef: MatDialogRef<CompanyModalComponent>) {
+  }
+  ngOnInit() {
+    this.setForm()
+  }
 
+  setForm() {
+    this.createCompanyForm = this.fb.group({
+      Id: [null],
+      CompanyName: ['', Validators.required],
+      Country: ['', Validators.required],
+      City: ['', Validators.required],
+      ContactPerson: ['', Validators.required],
+      OfficeContactNumber: ['', Validators.required],
+      MobileNumber: ['', Validators.required],
+      EmailAddress: ['', [Validators.required, Validators.email]],
+    });
+    this.createCompanyForm.patchValue({
+      Id: this.data.id,
+      CompanyName: this.data.companyName,
+      Country: this.data.country,
+      City: this.data.city,
+      ContactPerson: this.data.contactPerson,
+      OfficeContactNumber: this.data.officeContactNumber,
+      MobileNumber: this.data.mobileNumber,
+      EmailAddress: this.data.emailAddress,
+    })
+
+  }
+
+
+  addEmailAddress(v: any) {
+
+    if (!this.emailList.includes(v)) {
+      this.emailList.push(v)
+    }
+
+  }
+
+  removeEmailAddress(v: any) {
+
+    this.emailList.splice(v, 1)
+
+  }
+
+  onSubmit() {
+
+
+  }
+
+  onUpdate() {
+
+  }
+
+  close(): void {
+    this.dialogRef.close()
+  }
 }
