22589ef0cbe7522b14e428b3aa82ef5fadcc5fce
sheikharbab <arbabzafar4444@gmail.com>
2024-12-11 01:33:38 +0500

    f

--- src/app/dashboard-module/dashboard/dashboard.component.ts
+++ src/app/dashboard-module/dashboard/dashboard.component.ts
@@ -4,21 +4,32 @@ import { ApiCallingService } from "../../shared/Services/api-calling.service";
 import { ToastrService } from 'ngx-toastr';
 import { LocalStorageManagerService } from '../../shared/Services/local-storage-manager.service';
 import { AttendanceSummary, EmployeeLeaveSummary, ResDasSummary, TeamSummary } from "../../types/index";
+import { DpDatePickerModule } from 'ng2-date-picker';
+import { environment } from '../../../environments/environment.prod';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [CommonModule,TranslateModule,FormsModule,DpDatePickerModule,ReactiveFormsModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
+  datePickerConfig = {
+    format: environment.dateTimePatterns.date,
+  };
+
   attendanceSummary: AttendanceSummary;
   employeeLeaveSummary: EmployeeLeaveSummary;
   teamSummary: TeamSummary[];
   empId: string;
+
+  startDate = '';
+  endDate = new Date().toISOString();
 
   constructor(
     private api: ApiCallingService,
@@ -29,14 +40,18 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.fetchAttendanceData();
+    this.fetchAttendanceData({
+      startDate: this.startDate,
+      endDate: this.endDate,
+    });
   }
 
-  private fetchAttendanceData() {
-    this.api.getData("Dashboard", `getDashboardSummary`, true, { employeeId: this.empId }).subscribe({
+  private fetchAttendanceData({startDate,endDate}:{
+    startDate:string;
+    endDate: string;
+  }) {
+    this.api.getData("Dashboard", `getDashboardSummary`, true, { employeeId: this.empId,startDate,endDate }).subscribe({
       next: (response: ResDasSummary) => {
-        console.warn(response.data.teamSummary);
-
         if (response.success) {
           this.attendanceSummary = response.data.attendanceSummary;
           this.employeeLeaveSummary = response.data.employeeLeaveSummary;
@@ -51,6 +66,58 @@ export class DashboardComponent implements OnInit {
         this.teamSummary = [];
       }
     });
+  }
+
+
+  setFilter(option: string): void {
+    const today = new Date();
+    switch (option) {
+      case 'YTD': // Year to Date
+        this.startDate = new Date(today.getFullYear(), 0, 1).toISOString().split('T')[0];
+        this.endDate = today.toISOString().split('T')[0];
+        break;
+      case 'MTD': // Month to Date
+        this.startDate = new Date(today.getFullYear(), today.getMonth(), 1).toISOString().split('T')[0];
+        this.endDate = today.toISOString().split('T')[0];
+        break;
+      case 'QTD': // Quarter to Date
+        const currentQuarter = Math.floor(today.getMonth() / 3);
+        this.startDate = new Date(today.getFullYear(), currentQuarter * 3, 1).toISOString().split('T')[0];
+        this.endDate = today.toISOString().split('T')[0];
+        break;
+      case 'PreviousYear':
+        const lastYear = today.getFullYear() - 1;
+        this.startDate = new Date(lastYear, 0, 1).toISOString().split('T')[0];
+        this.endDate = new Date(lastYear, 11, 31).toISOString().split('T')[0];
+        break;
+      case 'PreviousMonth':
+        const firstDayPrevMonth = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+        const lastDayPrevMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+        this.startDate = firstDayPrevMonth.toISOString().split('T')[0];
+        this.endDate = lastDayPrevMonth.toISOString().split('T')[0];
+        break;
+      case 'Last7Days':
+        const lastWeek = new Date(today);
+        lastWeek.setDate(today.getDate() - 6); // Go back 6 days from today
+        this.startDate = lastWeek.toISOString().split('T')[0];
+        this.endDate = today.toISOString().split('T')[0];
+        break;
+      default:
+        break;
+    }
+  }
+  onStartDateChange(event: Event): void {
+    const selectedDate = (event.target as HTMLInputElement).value;
+    this.startDate = selectedDate; // Update local state
+  }
+
+  onEndDateChange(event: Event): void {
+    const selectedDate = (event.target as HTMLInputElement).value;
+    this.endDate = selectedDate; // Update local state
+  }
+
+  applyDateFilter(): void {
+
   }
 
 }
